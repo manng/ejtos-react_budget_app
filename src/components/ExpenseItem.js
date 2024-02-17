2bc92e51d5985de137079b37ae5296030f5fa315
Gary Mann <gary.mann1@optusnet.com.au>
2024-02-17 20:58:13 +1100

    Added plus and minus signs

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,1 +1,68 @@
+import React, { useContext } from 'react';
+import { TiDelete } from 'react-icons/ti';
+import { AppContext } from '../context/AppContext';
+import plus_sign from "../images/plus-sign.png";
+import minus_sign from "../images/minus-sign.png";
 
+const ExpenseItem = (props) => {
+    const { dispatch } = useContext(AppContext);
+
+	const imagesList = [
+	  {
+	    id: 1,
+	    src: plus_sign,
+	    alt: "Plus",
+	  },
+	  {
+	    id: 2,
+	    src: minus_sign,
+	    alt: "Minus",
+	  }
+	];
+
+    const handleDeleteExpense = () => {
+        dispatch({
+            type: 'DELETE_EXPENSE',
+            payload: props.id,
+        });
+    };
+
+    const increaseAllocation = (name) => {
+        const expense = {
+            name: name,
+            cost: 10,
+        };
+
+        dispatch({
+            type: 'ADD_EXPENSE',
+            payload: expense
+        });
+
+    }
+
+    const decreaseAllocation = (name) => {
+        const expense = {
+            name: name,
+            cost: 10,
+        };
+
+        dispatch({
+            type: 'RED_EXPENSE',
+            payload: expense
+        });
+
+    }
+
+    return (
+		
+        <tr>
+        <td>{props.name}</td>
+        <td>£{props.cost}</td>
+        <td><img src={plus_sign} height={20} width={20}  onClick={event=> increaseAllocation(props.name)}/></td>
+        <td><img src={minus_sign} height={20} width={20}  onClick={event=> decreaseAllocation(props.name)}/></td>
+        <td><TiDelete size='1.5em' onClick={handleDeleteExpense}></TiDelete></td>
+        </tr>
+    );
+};
+
+export default ExpenseItem;
